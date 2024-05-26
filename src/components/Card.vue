f76378228a59d1546b0f0211d8f043bf6525a7d1
Repoyy <revim1809@gmail.com>
2024-05-26 22:51:15 +0700

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="q-pa-md">
+      <div class="grid-wrapper q-gutter-md">
+        <q-card v-for="(card, index) in cards" :key="index" class="my-card">
+          <img :src="card.img" class="card-img">
+          <q-card-section>
+            <div class="text-h6">{{ card.title }}</div>
+            <div class="text-subtitle2">{{ card.harga }}</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none">
+            {{ card.description }}
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    setup() {
+      return {
+        cards: [
+          {
+            img: "src/assets/IMG-20240526-WA0019.jpg",
+            title: "Repp polo Shirt",
+            harga: "Rp 200.000",          },
+          {
+            img: "src/assets/IMG-20240526-WA0020.jpg",
+            title: "Repp polo Shirt",
+            harga: "Rp 200.000",
+          },
+          {
+            img: "src/assets/IMG-20240526-WA0021.jpg",
+            title: "Repp polo Shirt",
+            harga: "Rp 200.000",
+          },
+          {
+            img: "src/assets/IMG-20240526-WA0022.jpg",
+            title: "Repp polo Shirt",
+            harga: "Rp 200.000",
+          },
+          {
+            img: "src/assets/IMG-20240526-WA0023.jpg",
+            title: "Repp polo Shirt",
+            harga: "Rp 200.000",          },
+          {
+            img: "src/assets/IMG-20240526-WA0024.jpg",
+            title: "Repp polo Shirt",
+            harga: "Rp 200.000",
+          },
+          {
+            img: "src/assets/IMG-20240526-WA0025.jpg",
+            title: "Repp polo Shirt",
+            harga: "Rp 200.000",
+          },
+          {
+            img: "https://down-id.img.susercontent.com/file/id-11134207-7qula-li0d0hr3hxisb9",
+            title: "Repp kemeja",
+            harga: "Rp 200.000",
+          },
+          {
+            img: "https://down-id.img.susercontent.com/file/id-11134207-7qul7-li0d0hr3m584e1",
+            title: "Repp kemeja",
+            harga: "Rp 200.000",
+          },
+          // Four new cards
+          {
+            img: "https://down-id.img.susercontent.com/file/id-11134207-7qul8-li0d97ckqrmc67",
+            title: "Repp kemeja",
+            harga: "Rp 200.000",
+          }
+        ]
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .grid-wrapper {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    gap: 16px;
+  }
+  
+  .my-card {
+    width: 100%;
+    max-width: 100%;
+    background-image: linear-gradient(163deg, #01204E 10%, #028391 100%);
+    border-radius: 5px;
+    transition: all .3s;
+    cursor: pointer;
+  }
+  
+  .card-img {
+    width: 100%;
+    height: auto;
+  }
+  
+  .my-card:hover {
+    transform: scale(0.98);
+  }
+  
+  @media (max-width: 600px) {
+    .grid-wrapper {
+      grid-template-columns: 1fr;
+    }
+  
+    .my-card {
+      margin-bottom: 16px;
+    }
+  }
+  </style>
+  
---
+++ src/components/Carousel.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="q-pa-md">
+      <q-carousel
+        animated
+        v-model="slide"
+        arrows
+        navigation
+        infinite
+      >
+          <q-carousel-slide :name="1" img-src="src/assets/IMG-20240526-WA0019.jpg" class="carousel-img" />
+          <q-carousel-slide :name="2" img-src="https://down-id.img.susercontent.com/file/sg-11134201-22120-rohabay0oplv05" class="carousel-img" />
+          <q-carousel-slide :name="3" img-src="https://down-id.img.susercontent.com/file/sg-11134201-22120-y52btabtpplv95"class="carousel-img" />
+          <q-carousel-slide :name="4" img-src="https://down-id.img.susercontent.com/file/id-11134207-7qukw-lhxmo4v9a67e02" class="carousel-img" />
+      </q-carousel>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue'
+  
+  export default {
+    setup () {
+      return {
+        slide: ref(1)
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .carousel-img {
+    width: 100%; /* Menggunakan lebar 100% agar gambar terlihat responsif */
+    height: 1080px; /* Menggunakan ketinggian otomatis agar gambar tetap proporsional */
+    max-height: 100px; /* Atur ketinggian maksimum agar gambar tidak terlalu besar */
+    background-size: contain; /* Menggunakan contain untuk memastikan gambar tetap proporsional */
+    background-position: center; /* Posisikan gambar di tengah-tengah */
+  }
+  </style>
+  
